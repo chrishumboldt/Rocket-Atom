3daf057e449f7327dba4b9097b3c2a79ceb35e39
Chris Humboldt <chrismodem@gmail.com>
2019-09-03 09:36:55 +0800

    Actual initial commit.

---
+++ spec/rocket-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RocketAtomView from '../lib/rocket-atom-view';
+
+describe('RocketAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
